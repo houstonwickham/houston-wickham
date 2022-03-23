81ed54bba2aff4859b6de5a8aae99997d98afec3
houstonwickham <78935558+houstonwickham@users.noreply.github.com>
2022-03-22 20:13:31 -0500

    sidebar
    adding sidebar

--- houston-wickham/components/UI/Header.js
+++ houston-wickham/components/UI/Header.js
@@ -11,24 +11,30 @@ const Header = () => {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.logo}>
-          <Link href='/'>HW</Link>
+        <div className={styles.headerWrapper}>
+          <div className={styles.logo}>
+            <Link href='/'>HW</Link>
+          </div>
+          <nav>
+            <Link href='/projects'>PROJECTS</Link>
+            <Link href='/contact'>CONTACT</Link>
+          </nav>
+          <button onClick={() => setNavOpen(true)}>
+            <FaBars />
+          </button>
         </div>
-        <nav>
-          <Link href='/projects'>PROJECTS</Link>
-          <Link href='/contact'>CONTACT</Link>
-        </nav>
-        <button onClick={() => setNavOpen(true)}>
-          <FaBars />
-        </button>
       </header>
-      <nav className={navOpen ? styles.mobileNavActive : styles.mobileNav}>
+      <div className={navOpen ? styles.mobileNavActive : styles.mobileNav}>
         <div className={styles.mobileCloseHolder}>
           <button onClick={() => setNavOpen(false)}>
             <FaTimes />
           </button>
         </div>
-      </nav>
+        <ul className={styles.mobileNavList} onClick={() => setNavOpen(false)}>
+          <Link href='/projects'>PROJECTS</Link>
+          <Link href='/contact'>CONTACT</Link>
+        </ul>
+      </div>
     </>
   );
 };
--- houston-wickham/pages/_document.js
+++ houston-wickham/pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link
-            href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet'
             rel='stylesheet'
           />
         </Head>
